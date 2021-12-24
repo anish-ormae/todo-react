0e0136a81b88a422f8c76baa1060ebad71bf1ff1
Arun Yokesh <ayokesh.cse@gmail.com>
2021-12-24 19:16:15 +0530

    added subtask functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { Table, Switch, Col,Row } from "antd";
+import { Table, Switch, Col, Row, List, Typography } from "antd";
 import styles from "./App.css";
 const App = () => {
   const [tableDataSource, setTableDataSource] = useState([]);
   const [taskCompleted, setTaskCompleted] = useState([]);
+  const [subtaskCompleted, setSubtaskCompleted] = useState([]);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos?_start=0&_limit=10")
@@ -39,22 +40,86 @@ const App = () => {
     },
   ];
 
-  const toggleTask = (e, text, record, index) => {
-    setTaskCompleted(taskCompleted.concat(index));
+  const toggleTask = (e, text, record, index, subtask = false) => {
+    if (subtask) {
+      setSubtaskCompleted(
+        subtaskCompleted.concat(parseInt(`${record.id}${index}`))
+      );
+    } else {
+      setTaskCompleted(taskCompleted.concat(index));
+    }
   };
+
+  const subTasksData = [
+    {
+      id: 1,
+      title: "Racing car sprays burning fuel into crowd.",
+      parentId: 1,
+    },
+    {
+      id: 2,
+      title: "Los Angeles battles huge wildfires.",
+      parentId: 1,
+    },
+    {
+      id: 3,
+      title: "Los Angeles battles huge wildfires.",
+      parentId: 2,
+    },
+    {
+      id: 4,
+      title: "Los Angeles battles huge wildfires.",
+      parentId: 5,
+    },
+  ];
+
+  const getSubTasks = (record, index) => {
+    const { title } = record;
+    const getData = subTasksData
+      .filter((i) => i.parentId === record.id)
+      .map((i) => i.title);
+    console.log(subtaskCompleted);
+    return (
+      getData.length && (
+        <List
+          dataSource={getData}
+          renderItem={(item, subindex) => (
+            <List.Item>
+              <Switch
+                checked={subtaskCompleted.includes(
+                  parseInt(`${record.id}${subindex}`)
+                )}
+                disabled={subtaskCompleted.includes(
+                  parseInt(`${record.id}${subindex}`)
+                )}
+                onChange={(e) => toggleTask(e, title, record, subindex, true)}
+              />
+              {subtaskCompleted.includes(
+                parseInt(`${record.id}${subindex}`)
+              ) ? (
+                <del>{item}</del>
+              ) : (
+                <span>{item}</span>
+              )}
+            </List.Item>
+          )}
+        />
+      )
+    );
+  };
+
   return (
     <Row>
       <Col span={3}></Col>
       <Col span={18}>
-        <h1 style={{textAlign:'center'}}>TODO LIST</h1>
+        <h1 style={{ textAlign: "center" }}>TODO LIST</h1>
         <Table
           dataSource={tableDataSource}
           columns={columns}
           pagination={false}
           rowKey={(record) => record.id}
           expandable={{
-            expandedRowRender: record => <p style={{ margin: 0 }}>description</p>,
-            rowExpandable: record => record.name !== 'Not Expandable',
+            expandedRowRender: (record, index) => getSubTasks(record, index),
           }}
         />
       </Col>
